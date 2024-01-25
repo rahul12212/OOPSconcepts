9e07cd0a1da7cff2dbdd4834164c18bc59cde685
rahul12212 <122522969+rahul12212@users.noreply.github.com>
2024-01-25 12:08:06 +0530

    JAVA CLASS AND OBJECTS

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Book {
+   String title;
+    String author;
+    int pubdate;
+    double price;
+
+
+
+
+    void displayinfo(){
+        System.out.println(title + " | " + author + " | " + pubdate + " | " + price + " Rs");
+
+   }
+
+
+
+}
